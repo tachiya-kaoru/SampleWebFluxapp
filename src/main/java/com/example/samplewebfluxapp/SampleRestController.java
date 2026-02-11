91b9694a8bce06652798343081da2b81e51f939c
tachiya kaoru <picky.0703@gmail.com>
2026-02-11 16:44:45 +0900

    Monoの作成

--- src/main/java/com/example/samplewebfluxapp/SampleRestController.java
+++ src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -3,6 +3,8 @@ package com.example.samplewebfluxapp;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
 @RestController
 
 public class SampleRestController {
@@ -10,5 +12,10 @@ public class SampleRestController {
     @RequestMapping("/")
     public String hello() {
         return "Hello, Flux !";
+    }
+
+    @RequestMapping("/flux")
+    public Mono<String> flux() {
+        return Mono.just("Hello, Flux (Mono) .");
     }
 }
