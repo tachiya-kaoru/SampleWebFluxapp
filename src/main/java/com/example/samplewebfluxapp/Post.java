8469523d015064b94b1f0be33cc3add517c5f94f
tachiya kaoru <picky.0703@gmail.com>
2026-02-14 11:56:13 +0900

    IDでエンティティを取得する為SampleRestControllerを変更

--- src/main/java/com/example/samplewebfluxapp/Post.java
+++ src/main/java/com/example/samplewebfluxapp/Post.java
@@ -2,8 +2,7 @@ package com.example.samplewebfluxapp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,7 +10,7 @@ import jakarta.persistence.Table;
 @Table(name = "post")
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+   
     @Column
     public int id;
 
--- src/main/java/com/example/samplewebfluxapp/SampleRestController.java
+++ src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -7,6 +7,8 @@ import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 
@@ -28,11 +30,29 @@ public class SampleRestController {
     }
 
     @Autowired
-    PostRepository postRepository;
+    PostRepository repository;
 
     @RequestMapping("/post")
     public Mono<Post> post() {
         Post post = new Post(0,0,"dummy","dummy message...");
         return Mono.just(post);
     }
+
+    @RequestMapping("/post/{id}")
+    public Mono<Post> post(@PathVariable int id) {
+        Post post = repository.findById(id);
+        return Mono.just(post);
+    }
+
+    @PostConstruct
+    public void init() {
+        Post p1 = new Post(1,1,"Hello", "Hello FLux!");
+        Post p2 = new Post(2,2,"Sample", "This is sample post");
+        Post p3 = new Post(3,3,"ハロー", "これはサンプルです。");
+        repository.saveAndFlush(p1);
+        repository.saveAndFlush(p2);
+        repository.saveAndFlush(p3);
+    }
+
+   
 }
