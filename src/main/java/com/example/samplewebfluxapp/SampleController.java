38fb404f0e2e29a891733bfb4c1686c7baa29bf6
tachiya kaoru <picky.0703@gmail.com>
2026-02-17 22:59:48 +0900

    関数ルーティングを追加

--- src/main/java/com/example/samplewebfluxapp/SampleController.java
+++ src/main/java/com/example/samplewebfluxapp/SampleController.java
@@ -14,6 +14,8 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 
+import org.springframework.http.MediaType;
+
 import reactor.core.publisher.Mono;
 
 @Controller
@@ -26,12 +28,18 @@ public class SampleController {
     @Bean
     public RouterFunction<ServerResponse> routes() {
         return route(GET("/f/hello"), this::hello)
-        .andRoute(GET("/f/hello2"), this::hello2);
+        .andRoute(GET("/f/hello2"), this::hello2)
+        .andRoute(GET("/f/flux2"), this::flux2);
+    }
+
+    Mono<ServerResponse> flux2(ServerRequest req) {
+        return ok().contentType(MediaType.TEXT_HTML).render("flux");
     }
 
     Mono<ServerResponse> hello(ServerRequest req) {
         return ok().body(Mono.just("Hello Functional routing world!"), String.class);
     }
+
     Mono<ServerResponse> hello2(ServerRequest req) {
         return ok().body(Mono.just("関数ルーティングの世界へようこそ!"), String.class);
     }
